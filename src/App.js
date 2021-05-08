41de7b4e5912a45ebe2bce9d0d382cb5308fbf99
estherkimyunjung <estherkimyunjung@gmail.com>
2021-05-08 08:00:53 -0500

    add search component and styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
-import Header from "./Componet/Header.js";
+import Header from "./componet/Header.js";
+import Banner from "./componet/Banner.js";
+import Search from "./componet/Search.js";
 
 function App() {
   return (
@@ -8,10 +10,11 @@ function App() {
       {/* Header */}
       <Header />
       {/* Banner */}
+      <Banner />
       {/* Movies 
             // Search Movies
             // Awards Movies
-      */}
+          */}
       {/* Footer */}
     </div>
   );
--- src/Componet/Search.js
+++ src/Componet/Search.js
@@ -1,9 +1,15 @@
-import React from "react";
+import "./Search.css";
+import { Button } from "@material-ui/core";
+import MovieIcon from "@material-ui/icons/Movie";
 
 function Search() {
   return (
-    <div>
-      <h3>Search Movies Here</h3>
+    <div className="search">
+      <h2>
+        Esther's Movie Database <MovieIcon />
+      </h2>
+      <input type="text" placeholder="Enter Movie Title Here" />
+      <Button>Search Movies...</Button>
     </div>
   );
 }
