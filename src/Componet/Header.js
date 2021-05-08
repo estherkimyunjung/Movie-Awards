59a9571209236277dd30785cef2ce9b8376e51d3
estherkimyunjung <estherkimyunjung@gmail.com>
2021-05-08 05:14:19 -0500

    completed header styled

--- src/Componet/Header.js
+++ src/Componet/Header.js
@@ -1,10 +1,43 @@
 import React from "react";
 import "./Header.css";
+import HomeIcon from "@material-ui/icons/Home";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import SearchIcon from "@material-ui/icons/Search";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 
 function Header() {
   return (
     <div className="header">
-      <h3>Header Here</h3>
+      <div className="header__icons">
+        {/* All icons Here */}
+        <div className="header__icon header__icon--active">
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+        <div className="header__icon">
+          <FlashOnIcon />
+          <p>Trending</p>
+        </div>
+        <div className="header__icon">
+          <LiveTvIcon />
+          <p>verified</p>
+        </div>
+        <div className="header__icon">
+          <VideoLibraryIcon />
+          <p>Collection</p>
+        </div>
+        <div className="header__icon">
+          <SearchIcon />
+          <p>Search</p>
+        </div>
+        <div className="header__icon">
+          <PersonOutlineIcon />
+          <p>Login</p>
+        </div>
+      </div>
+      <img src="./logo425.png" alt="logo image" srcset="" />
     </div>
   );
 }
